db816fc5d5e5687758acff3c2a6404fa28adf6d8
technomanya <38087296+technomanya@users.noreply.github.com>
2020-02-25 13:55:13 +0300

    Plower project added as first commit

---
+++ Assets/MaskMaker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskMaker : MonoBehaviour
+{
+    public SpriteMask plowIt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(plowIt, transform.position, Quaternion.Euler(new Vector3(90, 0, 0)));
+    }
+}
