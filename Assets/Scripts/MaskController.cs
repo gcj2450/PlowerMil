a658424c32edd36cc0a0f80d838b6b8eb66984f1
technomanya <38087296+technomanya@users.noreply.github.com>
2020-02-26 16:31:52 +0300

    Mask controller

---
+++ Assets/Scripts/MaskController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskController : MonoBehaviour
+{
+    public Transform playerTruck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTruck = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceZ = playerTruck.position.z-transform.position.z;
+        if (Mathf.Abs(distanceZ) > 10)
+            Destroy(gameObject);
+    }
+}
