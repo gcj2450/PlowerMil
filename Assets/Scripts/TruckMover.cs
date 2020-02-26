477973ed482f94672ab59996bfbe67141c3c7225
technomanya <38087296+technomanya@users.noreply.github.com>
2020-02-26 18:24:57 +0300

    Truck rotation managed

--- Assets/Scripts/TruckMover.cs
+++ Assets/Scripts/TruckMover.cs
@@ -6,26 +6,68 @@ public class TruckMover : MonoBehaviour
 {
     public Camera cam;  //Drag the camera object here
     public float rotationSpeed = 8;  //This will determine max rotation speed, you can adjust in the inspector
-    public float moveSpeed = 10; // speed of the player
+    public float mainMoveSpeed = 10;
 
     float posX = 0;
     float rotationY=0;
+    float moveSpeed = 10; // speed of the player
     Vector3 mouseTouchPos;
 
+    private void Start()
+    {
+        moveSpeed = mainMoveSpeed;
+    }
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -3, 3), transform.position.y, transform.position.z);
 
-        if(Input.GetMouseButton(0))
-            RotateObject();
+        //if (Input.GetMouseButton(0))
+        //{
+        //    //RotateObject();
+        //    RotateInFrame();
+        //}
+        RotateInFrame();
+        Debug.Log(moveSpeed);
+
     }
     void RotateObject()
     {
-        //transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, Mathf.Clamp(transform.rotation.y, -45, 45), transform.rotation.z));
+        
         float angle = Input.GetAxis("Mouse X") * rotationSpeed ;
-        transform.Rotate(Vector3.up, angle*Time.deltaTime, Space.World);        
+        transform.Rotate(Vector3.up, angle*Time.deltaTime, Space.World);
+        //transform.eulerAngles.y = Mathf.Clamp(transform.eulerAngles.y, -45, 45);
     }
+    float ClampAngle(float angle, float from, float to)
+    {
+        // accepts e.g. -80, 80
+        if (angle < 0f) angle = 360 + angle;
+        if (angle > 180f) return Mathf.Max(angle, 360 + from);
+        return Mathf.Min(angle, to);
+    }
+    void RotateInFrame()
+    {
+        if (!Input.GetMouseButton(0)) return; // RMB down
 
+
+        float mx = Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
+        //float my = Input.GetAxis("Mouse Y") * Time.deltaTime * rotationSpeed;
+
+        Vector3 rot = transform.rotation.eulerAngles + new Vector3(0, mx, 0f); //use local if your char is not always oriented Vector3.up
+        rot.y = ClampAngle(rot.y, -30f, 30f);
+
+        transform.eulerAngles = rot;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.rigidbody.mass > 1)
+        {
+            moveSpeed = moveSpeed * GetComponent<Rigidbody>().mass / collision.rigidbody.mass;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        moveSpeed = mainMoveSpeed;
+    }
 }
