647be9c9749347e2f8b1e6cfb71ce4a6818d9606
burakdolanbay <burakdolanbay@gmail.com>
2020-03-04 17:04:39 +0300

    ball add

---
+++ Assets/Scenes/Scripts/BallController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject objGo;
+    public List<GameObject> winterBall = new List<GameObject>();
+    public int counter = 0;
+    public float distance = 0.0f;
+
+    void Awake()
+    {
+        for (int i = 0; i < counter; i++)
+        {
+            winterBall.Add(Instantiate(objGo));
+          // winterBall[i].transform.parent = this.transform;
+            winterBall[i].transform.position = new Vector3(Random.Range(-5f, 5f), objGo.transform.position.y, objGo.transform.position.z+ (i*20) );
+        }
+    }
+    void Start()
+    {
+        //for (int i = 0; i < counter; i++)
+        //{
+        //   
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scenes/Scripts/MaskMaker.cs
+++ Assets/Scenes/Scripts/MaskMaker.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class MaskMaker : MonoBehaviour
 {
     public SpriteMask plowIt;
+    public  bool winterPathtrigger;
+    private float lastGeneratePosZ = float.MinValue;
+    public float deltaValue = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,28 @@ public class MaskMaker : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (winterPathtrigger)
+        {
+
+            if (transform.position.z - deltaValue > lastGeneratePosZ)
+            {
+                lastGeneratePosZ = transform.position.z;
+                WinterPathShadow();
+            }
+
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Snow")
+        {
+            winterPathtrigger = true;
+        }
+    }
+
+    void WinterPathShadow()
     {
         Instantiate(plowIt, transform.position, Quaternion.Euler(new Vector3(90, 0, 0)));
     }
