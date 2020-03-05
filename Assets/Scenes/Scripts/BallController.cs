7eb0080f831dd744e75f0720e9d64168dd8caacf
burakdolanbay <burakdolanbay@gmail.com>
2020-03-05 14:52:09 +0300

    new texture

--- Assets/Scenes/Scripts/BallController.cs
+++ Assets/Scenes/Scripts/BallController.cs
@@ -9,14 +9,18 @@ public class BallController : MonoBehaviour
     public List<GameObject> winterBall = new List<GameObject>();
     public int counter = 0;
     public float distance = 0.0f;
+    public GameObject car;
 
     void Awake()
     {
         for (int i = 0; i < counter; i++)
         {
             winterBall.Add(Instantiate(objGo));
+           
           // winterBall[i].transform.parent = this.transform;
-            winterBall[i].transform.position = new Vector3(Random.Range(-5f, 5f), objGo.transform.position.y, objGo.transform.position.z+ (i*20) );
+            winterBall[i].transform.position = new Vector3(Random.Range(-5f, 5f), transform.position.y, transform.position.z+ (i*20) );
+            winterBall[i].SetActive(false);
+
         }
     }
     void Start()
@@ -28,8 +32,17 @@ public class BallController : MonoBehaviour
     }
 
     // Update is called once per frame
+    private int index = 0;
     void Update()
     {
-        
+        if (index<counter)
+        {
+            if (Vector3.Distance(car.transform.position, winterBall[index].transform.position) <= 30)
+            {
+                winterBall[index].SetActive(true);
+                index++;
+            }
+        }
+       
     }
 }
