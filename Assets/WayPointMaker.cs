36d4effd68a692dfe3e7f48e1739f695eddab0fe
technomanya <38087296+technomanya@users.noreply.github.com>
2020-04-03 18:43:11 +0300

    Rescue car takibi tamamlandı waypoint sistemi kullanıldı

---
+++ Assets/WayPointMaker.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointMaker : MonoBehaviour
+{
+    public float minDistance;
+    public Transform carPathPoint;
+    public Transform[] carPathList;
+
+    private Vector3 beforePos = Vector3.zero;
+    private Vector3 lastPos = Vector3.zero;
+    private int carPathPointIndex = 0;
+
+    private void Awake()
+    {
+
+        minDistance = Mathf.PI * 3.8f /3;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Snow"))
+        {
+            beforePos = transform.position;
+            lastPos = beforePos;
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.CompareTag("Snow"))
+        {
+            lastPos = transform.position;
+            if (Mathf.Abs(Vector3.Distance(lastPos, beforePos)) > minDistance)
+                PointMaker();
+        }
+
+    }
+
+    void PointMaker()
+    {
+
+        //Instantiate(transform, transform.position, Quaternion.identity);
+        if (carPathPointIndex < carPathList.Length)
+        {
+            carPathList[carPathPointIndex].position = transform.position;
+            carPathPointIndex++;
+        }
+        else
+        {
+            carPathPointIndex = 0;
+            return;
+        }
+
+        beforePos = transform.position;
+    }
+
+}
