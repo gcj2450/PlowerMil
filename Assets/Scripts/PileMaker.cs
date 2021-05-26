c4993b7b99eccac9d9f64593a1ccecccbe4f64bd
LAPTOP-Q2VQ7303\kanar <enesprlr@gmail.com>
2021-05-26 16:55:02 +0300

    ad

--- Assets/Scripts/PileMaker.cs
+++ Assets/Scripts/PileMaker.cs
@@ -74,6 +74,10 @@ public class PileMaker : MonoBehaviour
         }
 
     }
+    private void OnTriggerExit(Collider other)
+    {
+        winterPathtrigger = false;
+    }
 
 
     void PlacePile()
